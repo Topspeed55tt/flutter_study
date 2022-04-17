425685d519f74f9c5a6414ff1b4bca0001af9de4
Topspeed55tt <speed55tt@gmail.com>
2022-04-17 12:10:59 +0900

    flutter card practice ver1.1

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,14 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.teal,
           body: SafeArea(
               child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage("assets/sample.jpg"),
               ),
-              Text("SETSUNA F.SAYEI",
+              Text("IORIA",
                   style: TextStyle(
                     fontFamily: 'Pacifico',
                     fontSize: 40.0,
@@ -36,7 +38,55 @@ class MyApp extends StatelessWidget {
                   color: Colors.red,
                   letterSpacing: 2.5,
                 ),
-              )
+              ),
+              SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  )),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text('+81 xxx xxxxx xxx',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20.0,
+                          )),
+                    )),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Row(children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'xxxxxxxxxxxx@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ]),
+                ),
+              ),
             ],
           ))),
     );
